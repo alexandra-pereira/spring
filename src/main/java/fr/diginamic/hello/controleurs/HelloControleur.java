b7b7232cb552ab992f9baa4599379811ec96d639
alexandra-pereira <xandrapereira@hotmail.com>
2025-04-08 10:34:50 +0200

    tp 02

--- src/main/java/fr/diginamic/hello/controleurs/HelloControleur.java
+++ src/main/java/fr/diginamic/hello/controleurs/HelloControleur.java
@@ -1,5 +1,7 @@
 package fr.diginamic.hello.controleurs;
 
+import fr.diginamic.hello.services.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloControleur {
+
+    private final HelloService helloService;
+
+    // Injection par constructeur
+    @Autowired
+    public HelloControleur(HelloService helloService){
+        this.helloService=helloService;
+    }
+
     @GetMapping
     public String direHello (){
-        return "Hello";
+        return helloService.salutations();
     }
 }
